9d14a7a07cfb7c5500df21c0fdd7e4b5dbaa57d2
dubar <dubarzambrano2@gmail.com>
2025-06-11 16:25:11 -0400

    feat: add react-icons dependency; update Tarjeta and SongBar components for improved functionality and styling

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -2,14 +2,15 @@ import { $cancionActual } from '../store'
 import './SongBar.css'
 import React from 'react'
 
-export default function (){
+export default function () {
     const [cancionActual, setCancionActual] = React.useState(null)
 
     const audioRef = React.useRef(null)
 
     React.useEffect(() => {
-        $cancionActual.subscribe((song) => {
-            setCancionActual(song)
+        $cancionActual.subscribe((cancion) => {
+            setCancionActual(cancion)
+
         })
     }, [])
 
@@ -24,28 +25,32 @@ export default function (){
         }
     }
     return (
-        cancionActual && cancionActual.image && cancionActual.audio
-        ? <>
+        <>
             <div className='song-bar'>
-                <div className='song-bar__info'>
-                    <div className='song-bar__info__imge'>
-                        <img src={cancion.image.url} alt="" />
-                    </div>
-                    <div className='song-bar__info__details'>
-                        <h4 className='song-bar__info__details__title'>{cancionActual.title}</h4>
-                        <p className='song-bar__info__details__artist'>{cancionActual.artista}</p>
-                    </div>
-                </div>
-                <div className='song-bar__controls'>
-                    <div className='song-bar__controls__buttons'>
-                        <audio src={cancion.audio.url} autoPlay ref={audioRef}></audio>
-                    </div>
-                </div>
-                <div className='song-bar__extra' onClick={handlerClick}>
-                    <button>Play</button>
-                </div>
+                {
+                    cancionActual
+                        ? <>
+                            <div className='song-bar__info'>
+                                <div className='song-bar__info__imge'>
+                                    <img src={cancionActual.imgUrl} alt="" />
+                                </div>
+                                <div className='song-bar__info__details'>
+                                    <h4 className='song-bar__info__details__title'>{cancionActual.title}</h4>
+                                    <p className='song-bar__info__details__artist'>{cancionActual.artista}</p>
+                                </div>
+                            </div>
+                            <div className='song-bar__controls'>
+                                <div className='song-bar__controls__buttons'>
+                                    <audio src={cancionActual.audioUrl} autoPlay ref={audioRef}></audio>
+                                </div>
+                            </div>
+                            <div className='song-bar__extra' onClick={handlerClick}>
+                                <button>Play</button>
+                            </div>
+                        </>
+                        :<>No hay cancion seleccionada </>
+                }
             </div>
         </>
-            : <></>
     )
 }
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -1,27 +1,26 @@
+import { FaPlay } from "react-icons/fa";
 import { $cancionActual } from '../store'
 import './Tarjeta.css'
 
 export default function ({
-    imgUrl, artista, titulo, album, audioUrl
+    imgUrl, titulo, audioUrl
 }) {
 
 const handlerClick =() => {
     $cancionActual.set({
         imgUrl,
         titulo,
-        artista,
-        album,
         audioUrl
     })
 }
   
     return (
-        <div className='tarjeta' onClick={handlerClick}>
+        <div className='tarjeta'>
             <img src={imgUrl} alt={titulo} />
-            <h2>{artista}</h2>
+          
             <p>{titulo}</p>
-            <p>{album}</p>
-            <button onClick={handlerClick}>Play</button>
+            
+            <button onClick={handlerClick}><FaPlay /></button>
         </div>
     )
 }
