2abdcc0807f72713ff1ff31ff6ad4395cb467bc4
dubar <dubarzambrano2@gmail.com>
2025-06-11 16:45:03 -0400

    fix: simplify fallback message in SongBar component for clarity

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -48,7 +48,7 @@ export default function () {
                                 <button>Play</button>
                             </div>
                         </>
-                        :<>No hay cancion seleccionada, selecciona alguna </>
+                        :<>No hay cancion seleccionada </>
                 }
             </div>
         </>
